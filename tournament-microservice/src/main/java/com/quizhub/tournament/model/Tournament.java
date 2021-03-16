fb47a5bbe61855665a7e00fbe9fd29e5c344d7bc
fpoljcic <fpoljcic1@etf.unsa.ba>
2021-03-16 21:14:49 +0100

    Add tournament model

---
+++ tournament-microservice/src/main/java/com/quizhub/tournament/model/Tournament.java
@@ -0,0 +1,86 @@
+package com.quizhub.tournament.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class Tournament {
+
+    @Id
+    @Type(type = "uuid-char")
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime dateCreated;
+
+    @Column(nullable = false)
+    private LocalDateTime dateStart;
+
+    @Column(nullable = false)
+    private LocalDateTime dateEnd;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Tournament() {
+    }
+
+    public Tournament(UUID id, LocalDateTime dateCreated, LocalDateTime dateStart, LocalDateTime dateEnd, String name) {
+        this.id = id;
+        this.dateCreated = dateCreated;
+        this.dateStart = dateStart;
+        this.dateEnd = dateEnd;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public LocalDateTime getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(LocalDateTime dateStart) {
+        this.dateStart = dateStart;
+    }
+
+    public LocalDateTime getDateEnd() {
+        return dateEnd;
+    }
+
+    public void setDateEnd(LocalDateTime dateEnd) {
+        this.dateEnd = dateEnd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
