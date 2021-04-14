47cc2627795f25a07d6c175aeed393799a62535b
kkadusic <kkadusic1@etf.unsa.ba>
2021-04-14 22:37:26 +0200

    Set person service as gRPC client

--- person-microservice/src/main/java/com/quizhub/person/service/PersonService.java
+++ person-microservice/src/main/java/com/quizhub/person/service/PersonService.java
@@ -1,11 +1,19 @@
 package com.quizhub.person.service;
 
+import com.google.protobuf.Timestamp;
+import com.quizhub.person.event.EventRequest;
+import com.quizhub.person.event.EventResponse;
+import com.quizhub.person.event.EventServiceGrpc;
 import com.quizhub.person.exception.BadRequestException;
 import com.quizhub.person.exception.ConflictException;
 import com.quizhub.person.model.Person;
 import com.quizhub.person.repository.PersonRepository;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,32 +21,85 @@ import java.util.UUID;
 public class PersonService {
 
     private final PersonRepository personRepository;
+    private static String grpcUrl;
+    private static int grpcPort;
 
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
+    @Value("${app.grpc-url}")
+    public void setGrpcUrl(String grpcUrl) {
+        PersonService.grpcUrl = grpcUrl;
+    }
+
+    @Value("${app.grpc-port}")
+    public void setGrpcPort(int grpcPort) {
+        PersonService.grpcPort = grpcPort;
+    }
+
     public List<Person> getAllPersons() {
+        registerEvent(EventRequest.actionType.GET, "/api/person-ms/persons/all", "200");
         return personRepository.findAll();
     }
 
     public Person addPerson(Person person) {
-        if (personRepository.existsByUsernameIgnoreCase(person.getUsername())) {
-            throw new ConflictException("Username already taken.");
+        try {
+            if (personRepository.existsByUsernameIgnoreCase(person.getUsername())) {
+                throw new ConflictException("Username already taken.");
+            }
+            if (personRepository.existsByEmail(person.getUsername())) {
+                throw new ConflictException("Email already taken.");
+            }
+            registerEvent(EventRequest.actionType.CREATE, "/api/person-ms/persons", "200");
+            return personRepository.save(person);
+        } catch (ConflictException exception) {
+            registerEvent(EventRequest.actionType.CREATE, "/api/person-ms/persons", "409");
+            throw exception;
+        } catch (BadRequestException exception) {
+            registerEvent(EventRequest.actionType.CREATE, "/api/person-ms/persons", "400");
+            throw exception;
         }
-        if (personRepository.existsByEmail(person.getUsername())) {
-            throw new ConflictException("Email already taken.");
-        }
-        return personRepository.save(person);
     }
 
     public Person getPersonById(UUID id) {
-        return personRepository.findById(id).orElseThrow(() ->
-                new BadRequestException("Person with id " + id.toString() + " does not exist."));
+        registerEvent(EventRequest.actionType.GET, "/api/person-ms/persons", "200");
+        return personRepository.findById(id)
+                .orElseThrow(() -> {
+                    registerEvent(EventRequest.actionType.GET, "/api/person-ms/persons", "400");
+                    return new BadRequestException("Person with id " + id.toString() + " does not exist.");
+                });
     }
 
     public Person getPersonByUsername(String username) {
-        return personRepository.findByUsername(username).orElseThrow(() ->
-                new BadRequestException("Person with username " + username + " does not exist."));
+        registerEvent(EventRequest.actionType.GET, "/api/person-ms/persons/username", "200");
+        return personRepository.findByUsername(username)
+                .orElseThrow(() -> {
+                    registerEvent(EventRequest.actionType.GET, "/api/person-ms/persons/username", "400");
+                    return new BadRequestException("Person with username " + username + " does not exist.");
+                });
+    }
+
+    private void registerEvent(EventRequest.actionType actionType, String resource, String status) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(grpcUrl, grpcPort)
+                .usePlaintext()
+                .build();
+
+        EventServiceGrpc.EventServiceBlockingStub stub = EventServiceGrpc.newBlockingStub(channel);
+
+        Instant time = Instant.now();
+        Timestamp timestamp = Timestamp.newBuilder().setSeconds(time.getEpochSecond()).setNanos(time.getNano()).build();
+
+        EventResponse eventResponse = stub.log(EventRequest.newBuilder()
+                .setDate(timestamp)
+                .setMicroservice("Person service")
+                .setUser("Unknown")
+                .setAction(actionType)
+                .setResource(resource)
+                .setStatus(status)
+                .build());
+
+        System.out.println(eventResponse.getMessage());
+        channel.shutdown();
     }
 }
