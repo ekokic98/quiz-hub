31943a828176646aa3f29d3bfbe3325d385aef0c
kkadusic <kkadusic1@etf.unsa.ba>
2021-05-16 11:42:46 +0200

    Add initial login form

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,11 +1,60 @@
 import React from 'react';
+import {Form, Input, Button, Checkbox} from 'antd';
+import {UserOutlined, LockOutlined} from '@ant-design/icons';
 
 import "./login.scss";
 
 const Login = () => {
+
+    const onFinish = () => {
+        console.log('Received values of form.');
+    };
+
     return (
         <div className="login-container">
-            Login
+            <div className="login-title">
+                LOGIN
+            </div>
+            <Form
+                name="basic-login"
+                initialValues={{remember: true}}
+                size="large"
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    name="username"
+                    rules={[{required: true, message: 'Please input your username!'}]}
+                >
+                    <Input
+                        prefix={<UserOutlined className="site-form-item-icon"/>}
+                        placeholder="Username"
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    name="password"
+                    rules={[{required: true, message: 'Please input your password!'}]}
+                >
+                    <Input
+                        prefix={<LockOutlined className="site-form-item-icon"/>}
+                        type="password"
+                        placeholder="Password"
+                    />
+                </Form.Item>
+
+                <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" id="login-form-button">
+                        Log in
+                    </Button>
+                    Or <a href="">register now!</a>
+                </Form.Item>
+            </Form>
         </div>
     );
 }
