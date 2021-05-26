d5910a4d3b469feae7bbdff7febc5e29772a6d43
fpoljcic <fpoljcic1@etf.unsa.ba>
2021-05-26 23:42:45 +0200

    Fix getAllFavoritesByUser method

--- property-microservice/src/main/java/com/quizhub/property/repositories/FavoriteRepository.java
+++ property-microservice/src/main/java/com/quizhub/property/repositories/FavoriteRepository.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 @Repository
 public interface FavoriteRepository extends CrudRepository<Favorite, UUID> {
-    Optional<List<Favorite>> getFavoriteByPerson (UUID id);
+    List<Favorite> getFavoriteByPerson (UUID id);
     Optional<List<Favorite>> getFavoriteByQuiz(UUID id);
     boolean existsByQuizAndPerson (UUID q, UUID p);
 }
