ac4fdfc2ad11c765b957a8c49273b710c5c1971c
kkadusic <kkadusic1@etf.unsa.ba>
2021-05-27 01:56:59 +0200

    Enable generating users PDF report for admin

--- frontend/src/pages/Admin/index.js
+++ frontend/src/pages/Admin/index.js
@@ -1,6 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {getAllPersons} from "../../api/person/admin";
-import {message, Spin, Table} from "antd";
+import {Button, message, Spin, Table} from "antd";
+import jsPDF from "jspdf";
+import 'jspdf-autotable';
+import {DownloadOutlined} from "@ant-design/icons";
 
 const Admin = () => {
     const [persons, setPersons] = useState([]);
@@ -19,6 +22,31 @@ const Admin = () => {
         }
         fetchData();
     }, []);
+
+    const generateReport = () => {
+        let usersForReport = [];
+        for (let i = 0; i < persons.length; i++) {
+            let userObj = Object.values({
+                firstName: persons[i].firstName,
+                lastName: persons[i].lastName,
+                email: persons[i].email,
+                username: persons[i].username,
+                city: persons[i].city,
+                country: persons[i].country,
+                dateCreated: persons[i].dateCreated.substr(8, 2) + "." +
+                    persons[i].dateCreated.substr(5, 2) + "." +
+                    persons[i].dateCreated.substr(0, 4) + ".",
+                timeCreated: persons[i].dateCreated.substr(11, 5) + "h"
+            });
+            usersForReport.push(userObj);
+        }
+        const doc = new jsPDF();
+        doc.autoTable({
+            head: [['First name', 'Last name', 'Email', 'Username', 'City', 'Country', 'Date', 'Time']],
+            body: usersForReport
+        })
+        doc.save('QuizHub-Users.pdf');
+    };
 
     const columns = [
         {
@@ -73,7 +101,12 @@ const Admin = () => {
     return (
         <div>
             <h3> Users </h3>
-            {loading ? <Spin size="large"/> : <Table dataSource={persons} columns={columns}/>}
+            {loading ? <Spin size="large"/> :
+                <>
+                    <Table dataSource={persons} columns={columns}/>
+                    <Button onClick={generateReport}>PDF report<DownloadOutlined/></Button>
+                </>
+            }
         </div>
     );
 }
